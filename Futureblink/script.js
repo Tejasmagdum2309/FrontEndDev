c6559b65edb9eeec419aa00db0f7b0c974d60719
Tejasmagdum2309 <magdumtej1008@gmail.com>
2025-01-15 16:04:01 +0530

    Future blick front end ui

---
+++ Futureblink/script.js
@@ -0,0 +1,39 @@
+let menuToggle = document.getElementById("menu-toggle"); // Hamburger button
+let mobileMenu = document.getElementById("mobile-menu"); // Mobile menu
+let menuClose = document.getElementById("menu-close"); // Close button
+
+let tl_nav = gsap.timeline();
+
+tl_nav.from("#mobile-menu",{
+    y : "-100%",
+    opacity : 0,
+    duration : 0.6,
+    
+})
+
+tl_nav.from("#mobile-menu a",{
+    y : "100%",
+    opacity : 0,
+    duration : 0.6,
+    stagger : 0.2
+})
+
+tl_nav.from(menuClose,{
+    y : "100%",
+    opacity : 0,
+    duration : 0.6,
+    
+})
+
+
+tl_nav.pause();
+
+menuToggle.addEventListener('click',  () => {
+    tl_nav.play();
+})
+
+menuClose.addEventListener('click', () => {
+    tl_nav.reverse();
+
+    
+})
